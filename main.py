060c2b21d38f0839df7f5a47226f9ed980fd3799
Max <omaisjeg@tuta.io>
2019-02-14 14:58:44 +0100

    ca marche pas

--- main.py
+++ main.py
@@ -1,50 +1,45 @@
+#!/usr/bin/env python3
+
 from threading import Thread
 import tkinter as Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from tkinter.ttk import Frame
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from queue import Queue
 from time import sleep
+import numpy as np
+from random import uniform 
+import serial
+import struct
+
+ser = serial.Serial(port='/dev/ttyACM0',baudrate = 57600,timeout=0.01)
+
+sleep(0.2)
+# reçoie 40 octets (10 float32) : odo(temps,x,y,theta) consigne(vg,vd) commande(vg,vd) vitesse(vg,vd)
+taillerec = 40
+tailleenv = 16
+# envoie 16 octets (4 float32) : commande(temps,x,y,theta)
 
 
-from random import uniform # pour tester le code 
+def nothing(rien=0):
+    pass
 
-class Traj():
-    def __init__(self):
-        self.t = []
-        self.X = []
-        self.Y = []
-        self.theta = []
-        self.coord = [self.t,self.X,self.Y,self.theta]
+def threadfedtruk(threadname, q, port):
+    #read variable "a" modify by thread 2
+    print("faut mettre des trucs")
+    #TODO recuperation des infos du serial
+    t= 0
+    while 1:
+        sleep(0.1)
+        stuff = [[],[]]
 
-    def addPoint(self,arg):
-        # arg =  t,x,y,theta
-        for i in range(len(arg)):
-            self.coord[i].append(arg[i])
+        stuff[0].append([t,uniform(0,3),uniform(0,2),uniform(0,360)])
+        stuff[1].append([t,uniform(0,3),uniform(0,2),uniform(0,360)])
 
+        q.put(stuff)
 
-class Communication(Thread):
-    def __init__(self,SerialReference):
-        Thread.__init__(self)
-        self.serialReference = SerialReference
-        self.t = 0
-    def run(self):
-
-
-        print("faut mettre des trucs")
-        #TODO recuperation des infos du serial
-        
-
-        while 1:
-            sleep(0.1)
-            trajs[0].addPoint([self.t,uniform(0,3),uniform(0,2),uniform(0,360)])
-            trajs[1].addPoint([self.t,uniform(0,3),uniform(0,2),uniform(0,360)])
-
-            self.t += 1
-
-
-    # def sendFactorToMCU(self):
-    #     self.serialReference.sendSerialData(self.entry.get() + '%')     # '%' is our ending marker
+        t += 1
 
 class Window(Frame):
     def __init__(self, figure, master):
@@ -70,49 +65,85 @@ class Window(Frame):
         SendButton.pack(padx=5)
 
 
+def threadcomenvoie(threadname, q, port):
+    #read variable "a" modify by thread 2
 
+    #TODO recuperation des infos du serial
+    t= 0
+    while 1:
+        if q.qsize() > 10:
+            with q.mutex:
+                q.queue.clear()
+        q.get()
 
-print("ok")
+        stuff[0].append([t,uniform(0,3),uniform(0,2),uniform(0,360)])
+        stuff[1].append([t,uniform(0,3),uniform(0,2),uniform(0,360)])
 
-def nothing(rien=0):
-    pass
+        q.put(stuff)
 
-class Fenetre(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        
-    def run(self):
-
-        print("run")
-
-        fig = plt.figure()
-
-        root = Tk.Tk()
-        app = Window(fig, root)
-        ax = plt.axes(xlim=(-1,4),ylim=(-1,3))
-        ax.grid(True, linestyle='-')
-        lines = []
-
-        for i in range(2):
-            lines.append(ax.plot(trajs[i].X[-50:-1], trajs[i].Y[-50:-1]))
-
-        anim = animation.FuncAnimation(fig, self.refresh,fargs=(lines, ax),interval=100)
-
-        root.mainloop()   # use this instead of plt.show() since we are encapsulating everything in Tkinter
-        s.close()
-    def refresh(self,frame,lines,ax):
-        for i in range(2):
-            lines[i][0].set_xdata(trajs[i].X)
-            lines[i][0].set_ydata(trajs[i].Y)
+        t += 1
+        print("envoie ",t)
+        sleep(0.1)
 
 
 
-testodoTraj, testcommandTraj = Traj(),Traj()
-trajs = [testodoTraj, testcommandTraj]
+def threadcomrecoie(threadname, q, port):
+    #TODO recuperation des infos du serial
+    t= 0
+    while 1:
+        sleep(0.01)
 
-interface = Fenetre()
-arduino = Communication(0)
+        while port.in_waiting > 9:
+
+            t += 1
+            read_serial= ser.read(size=taillerec)
+            read = struct.unpack('10f', read_serial)
 
 
-arduino.start()
-interface.start()
+
+
+def threadinter(threadname, q):
+    print("run")
+
+    fig = plt.figure()
+
+    root = Tk.Tk()
+    app = Window(fig, root)
+    ax = plt.axes(xlim=(-1,4),ylim=(-1,3))
+    ax.grid(True, linestyle='-')
+    lines = []
+    newdata = np.array(q.get())
+    print(newdata.shape)
+    print("blabla")
+    for i in range(2):
+        lines.append(ax.plot([],[]))
+
+    anim = animation.FuncAnimation(fig, refresh,fargs=(lines, ax, newdata),interval=1000)
+
+    root.mainloop()  
+
+
+
+
+def refresh(frame,lines,ax,newdata):
+
+    for i in range(2):
+        lines[i][0].set_xdata(newdata[i,0,1])
+        lines[i][0].set_ydata(newdata[i,0,2])
+        newdata = lines[i][0].get_xdata(orig=True)
+        newdata = lines[i][0].get_ydata(orig=True)
+        print(newdata)
+
+varsharefromard = Queue()
+varsharetoard = Queue()
+
+comenv = Thread( target=threadcomenvoie, args=("Communication envoie", varsharetoard, ser) )
+comrec = Thread( target=threadcomrecoie, args=("Communication recoie", varsharefromard, ser) )
+inter = Thread( target=threadinter, args=("Interface", varsharefromard) )
+fedtruck = Thread( target=threadinter, args=("Interface", varsharefromard) )
+
+threads = [comenv,comrec,inter,fedtruck]
+
+for thr in threads:
+    thr.start()
+    thr.join()
